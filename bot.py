076702a3f7286485be497009f11be381a23131c2
Soham Chatterjee <96soham96@gmail.com>
2020-03-21 09:27:59 +0800

    Code Cleanup

--- bot.py
+++ bot.py
@@ -13,50 +13,35 @@ def get_fact():
         "| [Info](https://github.com/soham96/AlpacaBot/blob/master/README.md)"
         "| [Code](https://github.com/soham96/AlpacaBot)"
         "| [Feedback](http://np.reddit.com/message/compose/?to=JustAnAlpacaBot&subject=Feedback)"
-        "| [Contribute Fact](http://np.reddit.com/message/compose/?to=JustAnAlpacaBot&subject=Fact)")
-        #"\n \n ____ \n \n"
-        #"\n \n"
-        #"\n \n If you liked this fact, consider donating [here](https://github.com/soham96/AlpacaBot/blob/master/README.md)")
-    
+        "| [Contribute Fact](http://np.reddit.com/message/compose/?to=JustAnAlpacaBot&subject=Fact)"
+        "\n \n ###### You don't get a fact, you earn it. If you got this fact then AlpacaBot thinks you deserved it!")
+
     return text
 
 def get_comments():
     print(time.time())
 
     try:
-        for comment in reddit.subreddit('all').stream.comments(skip_existing=False):
+        subreddit=reddit.subreddit('all')
+        for comment in subreddit.stream.comments(skip_existing=False):
             if comment.author == 'JustAnAlpacaBot':
                 continue
             if 'alpaca' in comment.body.lower():
-                    reply_alpaca(comment.id, 'comment')
+                    reply_alpaca(comment)
                     print(time.time())
     except KeyboardInterrupt:
         raise
     except Exception as e:
         print(e)
-        pass
-        
+        pass        
 
-def reply_alpaca(post_id, post_type):
-    
-    if post_type=='post':
-        submission=reddit.submission(id=post_id)
-        try:
-            submission.reply(get_fact())
-            print(f"Commented on {post_type} {post_id} and url {submission.permalink}")
-        except:
-            time.sleep(600)
-            submission.reply(get_fact())
-            print(f"Commented on {post_type} {post_id} and url {submission.permalink}")
-    
-    if post_type=='comment':
-        comment=reddit.comment(id=post_id)
-        try:
-            comment.reply(get_fact())
-            print(f"Commented on {post_type} with {post_id} and url {comment.permalink}")
-        except:
-            print(f"Could not comment {post_id}")
+def reply_alpaca(comment):
 
+    try:
+        comment.reply(get_fact())
+        print(f"Commented on {comment.id} and url {comment.permalink}")
+    except:
+        print(f"Could not comment {comment.id}")
 def main(reddit):
     while True:
         get_comments()
