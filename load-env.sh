b20f2e32c08adcef04d4b45377a00bd7405db126
Soham Chatterjee <96soham96@gmail.com>
2021-05-28 16:35:39 +0800

    Thanks for fix Zack

--- load-env.sh
+++ load-env.sh
@@ -6,4 +6,4 @@ sed -i "s/<TAG>/$GITHUB_SHA/g" *
 sed -i "s/<PRAW_CLIENT_ID>/$PRAW_CLIENT_ID/g" *
 sed -i "s/<PRAW_CLIENT_SECRET>/$PRAW_CLIENT_SECRET/g" *
 sed -i "s/<PRAW_PASSWORD>/$PRAW_PASSWORD/g" *
-sed -i "s/<PRAW_USER_AGENT>/$PRAW_USER_AGENT/g" *
+sed -i "s|<PRAW_USER_AGENT>|$PRAW_USER_AGENT|g" *
