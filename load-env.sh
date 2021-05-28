ba2589de15d90ec34a2f180666c832fb004e932c
zack <zack.zhong@sleek.com>
2021-05-28 16:02:54 +0800

    fix

--- load-env.sh
+++ load-env.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 cd k8s/
-gsed -i "s/<PROJECT_ID>/$PROJECT_ID/g" *
-gsed -i "s/<IMAGE>/$IMAGE/g" *
-gsed -i "s/<TAG>/$GITHUB_SHA/g" *
-gsed -i "s/<PRAW_CLIENT_ID>/$PRAW_CLIENT_ID/g" *
-gsed -i "s/<PRAW_CLIENT_SECRET>/$PRAW_CLIENT_SECRET/g" *
-gsed -i "s/<PRAW_PASSWORD>/$PRAW_PASSWORD/g" *
-gsed -i "s/<PRAW_USER_AGENT>/$PRAW_USER_AGENT/g" *
+sed -i "s/<PROJECT_ID>/$PROJECT_ID/g" *
+sed -i "s/<IMAGE>/$IMAGE/g" *
+sed -i "s/<TAG>/$GITHUB_SHA/g" *
+sed -i "s/<PRAW_CLIENT_ID>/$PRAW_CLIENT_ID/g" *
+sed -i "s/<PRAW_CLIENT_SECRET>/$PRAW_CLIENT_SECRET/g" *
+sed -i "s/<PRAW_PASSWORD>/$PRAW_PASSWORD/g" *
+sed -i "s/<PRAW_USER_AGENT>/$PRAW_USER_AGENT/g" *
